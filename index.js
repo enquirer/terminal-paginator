309a5f3aece8c34cf6a1488d61443b7bbb2caa23
jonschlinkert <jon.schlinkert@sellside.com>
2016-09-21 06:14:57 -0400

    improve pagination rendering

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var flatten = require('arr-flatten');
+var debug = require('debug')('terminal-paginator');
 var log = require('log-utils');
 
 /**
@@ -10,33 +10,40 @@ var log = require('log-utils');
  */
 
 function Paginator(options) {
+  debug('initializing from <%s>', __filename);
   this.options = options || {};
   this.lastIndex = 0;
   this.pointer = 0;
 }
 
 Paginator.prototype.paginate = function(output, selected, pageSize) {
+  var m = output.match(/^\n+/);
+  var prefix = m ? m[0] : '';
+
+  output = output.replace(/^\n+/, '');
   pageSize = pageSize || this.options.pageSize || 7;
   var lines = output.split('\n');
 
-  // Make sure there's enough lines to paginate
-  if (lines.length <= pageSize + 2) {
+  // return if there are not enough lines to paginate
+  if (pageSize >= lines.length - 2) {
     return output;
   }
 
   // Move the pointer when a down keypress is entered, and limit it to 3
-  if (this.pointer < 3 && this.lastIndex < selected && selected - this.lastIndex < 9) {
+  if (this.pointer < 3 && this.lastIndex < selected && (selected - this.lastIndex) < 9) {
     this.pointer = Math.min(3, this.pointer + selected - this.lastIndex);
   }
 
   this.lastIndex = selected;
 
   // Duplicate the lines so it give an infinite list look
-  var infinite = flatten([lines, lines, lines]);
+  var infinite = lines.concat(lines).concat(lines);
   var topIndex = Math.max(0, selected + lines.length - this.pointer);
 
   var section = infinite.splice(topIndex, pageSize).join('\n');
-  return section + '\n' + log.dim('(Move up and down to reveal more choices)');
+  section += '\n';
+  section += log.dim('(Move up and down to reveal more choices)');
+  return prefix + section;
 };
 
 /**
